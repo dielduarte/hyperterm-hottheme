21a0738bf8fa0c02b8064bcfd6c07d2635bca8fd
magdielduarte <magdiel.duarte@hotmart.com.br>
2016-08-05 18:05:26 -0300

    added a border color const

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 const foregroundColor = '#eff0eb';
 const backgroundColor = '#053d4e';
 const cursorColor = '#f04e23';
+const borderColor = '#222430';
 
 exports.decorateConfig = config => Object.assign({}, config, {
 	backgroundColor,
 	foregroundColor,
-	borderColor: '#222430',
+	borderColor: borderColor,
 	cursorColor: cursorColor,
 	css: `
 		${config.css}
